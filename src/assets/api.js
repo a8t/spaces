aa3c06e6d4507435ad6c17e68ef25fef1f7d91a8
Andy Tran <andy8tran@gmail.com>
2018-06-01 22:35:23 -0400

    Remove unneeded files

--- src/assets/api.js
+++
@@ -1,6 +0,0 @@
-async function getPageData(pageNum) {
-  const response = await fetch(`https://thisopenspace.com/lhl-test?page=${pageNum}`);
-  const responseObj = await response.json();
-  const resultList = await responseObj.data;
-  return resultList;
-}
